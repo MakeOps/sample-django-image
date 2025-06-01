c1f4269142b0dc0e3340d9026875e7f8aa522f07
Jason Forte <hello@jforte.me>
2025-06-01 17:18:46 +0000

    Debugging deployment

--- app/sample/aws.py
+++ app/sample/aws.py
@@ -7,4 +7,5 @@ ECS_CONTAINER_METADATA_URI_V4 = os.environ.get("ECS_CONTAINER_METADATA_URI_V4")
 
 def get_fargate_ip_address():
     response = httpx.get(f"{ECS_CONTAINER_METADATA_URI_V4}/task")
+    print(response.json())
     return response.json()["Networks"][0]["IPv4Addresses"][0]
