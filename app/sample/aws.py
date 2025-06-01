f8c9a08a93ca849cf7ce8cc3d5dd1032347605f5
Jason Forte <hello@jforte.me>
2025-06-01 17:36:12 +0000

    Updated finding the network

--- app/sample/aws.py
+++ app/sample/aws.py
@@ -6,5 +6,11 @@ ECS_CONTAINER_METADATA_URI_V4 = os.environ.get("ECS_CONTAINER_METADATA_URI_V4")
 
 def get_fargate_ip_address():
     response = requests.get(f"{ECS_CONTAINER_METADATA_URI_V4}/task")
-    print(response.json())
-    return response.json()["Networks"][0]["IPv4Addresses"][0]
+    response_json = response.json()
+
+    web_container = [container for container in response_json["Containers"] if container["Name"] == "web"][0]
+    return web_container["Networks"][0]["IPv4Addresses"][0]
+
+
+if __name__ == "__main__":
+    print(get_fargate_ip_address())
