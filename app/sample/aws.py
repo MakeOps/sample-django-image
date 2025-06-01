8b272625628f3e3f27bc3022689aac0c0b5ae93d
Jason Forte <hello@jforte.me>
2025-06-01 17:23:38 +0000

    Move from httpx to requests

--- app/sample/aws.py
+++ app/sample/aws.py
@@ -1,11 +1,10 @@
 import os
-import httpx
-
+import requests
 
 ECS_CONTAINER_METADATA_URI_V4 = os.environ.get("ECS_CONTAINER_METADATA_URI_V4")
 
 
 def get_fargate_ip_address():
-    response = httpx.get(f"{ECS_CONTAINER_METADATA_URI_V4}/task")
+    response = requests.get(f"{ECS_CONTAINER_METADATA_URI_V4}/task")
     print(response.json())
     return response.json()["Networks"][0]["IPv4Addresses"][0]
