7d8d40e73340e837cf06b580afa203f04a2f8126
Jason Forte <hello@jforte.me>
2025-06-16 20:07:52 +0000

    Adding the dir

--- hack/start_server.sh
+++ hack/start_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-/app/.venv/bin/python manage.py collectstatic --noinput
+/app/.venv/bin/python app/manage.py collectstatic --noinput
 
 exec /app/.venv/bin/uvicorn --app-dir app sample.asgi:application --host 0.0.0.0 --port 8000
